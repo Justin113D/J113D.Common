14bbb46bf2e948e5a1fd6aeaf3bca52aa3fd4067
Justin113D <Justin113D@gmail.com>
2025-01-14 22:58:30 +0100

    Synced namespace

--- src/J113D.Common/DistinctMap.cs
+++ src/J113D.Common/DistinctMap.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
-namespace HEIO.NET
+namespace J113D.Common
 {
 	/// <summary>
 	/// A distinct index to value mapping.
